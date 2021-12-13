35873abbfdeb69c25450059b1a6d66220ea29037
Vlad Rusnak <vladrusnak2@outlook.com>
2021-12-13 04:44:49 +0200

    AddContextMenuFunctionalityToAllAdsTab

--- HouseProjectWpfApi/ViewModel/FindAdViewModel.cs
+++ HouseProjectWpfApi/ViewModel/FindAdViewModel.cs
@@ -22,6 +22,8 @@ namespace HouseProjectWpfApi.ViewModel
         }
 
         public AdShortInfoDTO SelectedAdFromAll { get; set; }
+
+        #region COMMANDS
         private BaseCommand getAllAds;
         public BaseCommand GetAllAds
         {
@@ -33,5 +35,42 @@ namespace HouseProjectWpfApi.ViewModel
                 });
             }
         }
+
+        private BaseCommand viewDetailOneFromAllAdInfo;
+        public BaseCommand ViewDetailOneFromAllAdInfo
+        {
+            get
+            {
+                return viewDetailOneFromAllAdInfo ?? new BaseCommand(obj =>
+                {
+                    OpenAdInfoWindow(SelectedAdFromAll.Id);
+                });
+            }
+        }
+
+        private BaseCommand adToComparasionFromAll;
+        public BaseCommand AdToComparasionFromAll
+        {
+            get
+            {
+                return adToComparasionFromAll ?? new BaseCommand(obj =>
+                {
+                    forCompareServices?.SetForCompare(UserProfile?.Id, SelectedAdFromAll.Id);
+                });
+            }
+        }
+
+        private BaseCommand adToFavoriteFromAll;
+        public BaseCommand AdToFavoriteFromAll
+        {
+            get
+            {
+                return adToFavoriteFromAll ?? new BaseCommand(obj =>
+                {
+                    favoritesServices?.SetFavorite(UserProfile?.Id, SelectedAdFromAll.Id);
+                });
+            }
+        }
+        #endregion
     }
 }
